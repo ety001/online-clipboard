756f43feffef271e83df27986432eb602e0a03ca
ETY001 <ety001@domyself.me>
2015-06-20 04:36:02 +0800

    syntax error

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ return array(
         'host'  => ($_ENV['DB_PORT_6379_TCP_ADDR'])?$_ENV['DB_PORT_6379_TCP_ADDR']:'localhost',
         'port'  => ($_ENV['DB_PORT_6379_TCP_PORT'])?$_ENV['DB_PORT_6379_TCP_PORT']:'6379',
         'db'    => ($_ENV['DB_NAME'])?$_ENV['DB_NAME']:'',
-        'pass'  => ($_ENV['REDIS_PASSWORD'])?$_ENV['REDIS_PASSWORD']:'';
+        'pass'  => ($_ENV['REDIS_PASSWORD'])?$_ENV['REDIS_PASSWORD']:''
     )
 );
