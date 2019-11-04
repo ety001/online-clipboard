a9a93976e2f4486d4526aa70ee79f3f45638a497
ETY001 <ety001@domyself.me>
2019-11-04 09:54:24 +0800

    update

--- server.php
+++ server.php
@@ -18,55 +18,65 @@ $ws = new swoole_websocket_server("0.0.0.0", 8080);
 
 $ws->on('open', function ($ws, $request) {
     global $redis;
-    $req    = $request->server;
-    $uri    = explode('/', $req['request_uri']);
-    $cb_name    = $uri[1];
-    $cb_pass    = $uri[2];
-    $hash   = md5($cb_pass. $cb_name);
+    try {
+        $req    = $request->server;
+        $uri    = explode('/', $req['request_uri']);
+        $cb_name    = $uri[1];
+        $cb_pass    = $uri[2];
+        $hash   = md5($cb_pass. $cb_name);
 
-    //if fd is 1, the server had been restarted
-    if($request->fd==1){
-        hash_clear($redis);
+        //if fd is 1, the server had been restarted
+        if($request->fd==1){
+            hash_clear($redis);
+        }
+
+        var_dump($hash,$request->fd);
+
+        //save $fd => $hash
+
+        if($redis->hGet('fd.to.hash', $request->fd)!=$hash){
+            $redis->hSet('fd.to.hash', $request->fd, $hash);
+        }
+        //save $hash => $fd, this list be used to publish msg on the channel. 
+        $redis->lPush('publish_'.$hash, $request->fd);
+
+        $all    = $redis->lRange($hash, 0, -1);
+        $all    = array_reverse($all);
+        $result = array('type'=>'all', 'data'=>$all);
+        $ws->push($request->fd, json_encode($result));
+    } catch(\Exception $e) {
+        var_dump('Error:'.$e->getMessage());
     }
-
-    var_dump($hash,$request->fd);
-
-    //save $fd => $hash
-
-    if($redis->hGet('fd.to.hash', $request->fd)!=$hash){
-        $redis->hSet('fd.to.hash', $request->fd, $hash);
-    }
-    //save $hash => $fd, this list be used to publish msg on the channel. 
-    $redis->lPush('publish_'.$hash, $request->fd);
-
-    $all    = $redis->lRange($hash, 0, -1);
-    $all    = array_reverse($all);
-    $result = array('type'=>'all', 'data'=>$all);
-    $ws->push($request->fd, json_encode($result));
+    
 });
 
 $ws->on('message', function ($ws, $frame) {
     global $redis;
-    $hash       = $redis->hGet('fd.to.hash', $frame->fd);
-    $r          = json_decode($frame->data, true);
-    if(empty($r))return;
-    $msg        = $r['msg'];
-    if($msg==='')return;
-    switch ($r['type']) {
-        case 'message':
-            save_cb($redis, $hash, $msg);
-            publish($redis, $hash, $ws, $msg);
-            var_dump($hash, $msg);
-            break;
-        case 'ping':
-            publish($redis, $hash, $ws, [
-                'type' => 'pong', 'msg' => 'pone',
-            ], true);
-            break;
-        default:
-            var_dump('unknown type: '.$frame->data);
-            break;
+    try {
+        $hash       = $redis->hGet('fd.to.hash', $frame->fd);
+        $r          = json_decode($frame->data, true);
+        if(empty($r))return;
+        $msg        = $r['msg'];
+        if($msg==='')return;
+        switch ($r['type']) {
+            case 'message':
+                save_cb($redis, $hash, $msg);
+                publish($redis, $hash, $ws, $msg);
+                var_dump($hash, $msg);
+                break;
+            case 'ping':
+                publish($redis, $hash, $ws, [
+                    'type' => 'pong', 'msg' => 'pone',
+                ], true);
+                break;
+            default:
+                var_dump('unknown type: '.$frame->data);
+                break;
+        }
+    } catch (\Exception $e) {
+        var_dump('Error:'.$e->getMessage());
     }
+    
 });
 
 $ws->on('close', function ($ws, $fd) {
